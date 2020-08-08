fe02028ab23eff55918a00b72d0b81061f7d2e39
Amit Tomar <49426082+amittomar-1@users.noreply.github.com>
2020-08-08 10:39:02 +0530

    Add files via upload

---
+++ precache-manifest.50a65641aeb1262b796841765da99a1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1bd8f531b96d399124d",
+    "url": "/static/css/main.88353733.chunk.css"
+  },
+  {
+    "revision": "d1bd8f531b96d399124d",
+    "url": "/static/js/main.d1bd8f53.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "c2869453d75fc28725aa",
+    "url": "/static/css/2.f859875f.chunk.css"
+  },
+  {
+    "revision": "c2869453d75fc28725aa",
+    "url": "/static/js/2.c2869453.chunk.js"
+  },
+  {
+    "revision": "30c884214130ee61203af8886215139b",
+    "url": "/index.html"
+  }
+];
